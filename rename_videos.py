39a48ef502bd52b6555d58e6e3de55bea472ee53
Tyler Sellon <c-tylers@vulcan.com>
2016-08-30 11:41:37 -0700

    Adds support for L & R camera.

--- rename_videos.py
+++ rename_videos.py
@@ -28,30 +28,53 @@ def rename_videos(video_dir, excel_file, output_folder):
         trip_code = get_cell(row, 'trip_code').value
         set_code = get_cell(row, 'set_code').value
         video = get_cell(row, 'video').value
-        camera = get_cell(row, 'camera').value
+        try:
+            camera = get_cell(row, 'camera').value
+        except KeyError:
+            camera = None
         if trip_code and set_code:
             logging.info('Finding video for trip: {}, set: {}'.format(trip_code, set_code))
             if video:
                 video_path = '{}/{}'.format(video_dir, video)
                 video_candidates = [xx for xx in os.walk(video_path) if 'joined.mp4' in xx[2]]
-                if len(video_candidates) > 1:
-                    logging.error('More than one video found for this set.')
+                if len(video_candidates) == 2:
+                    camera_types = ['L', 'R']
+                    processed = {}
+                    for ct in camera_types:
+                        processed[ct] = False
+                    for camera in camera_types:
+                        for candidate in video_candidates:
+                            if os.path.basename(candidate[0]).startswith(camera):
+                                if processed[camera]:
+                                    logging.error('Second {} camera video found: {}'.format(camera, candidate[0]))
+                                else:
+                                    orig_video_path = '{}/joined.mp4'.format(candidate[0])
+                                    rename_video(trip_code, set_code, camera, output_folder, orig_video_path)
+                                    processed[camera] = True
+                elif len(video_candidates) > 1:
+                    logging.error('More than two videos found for this set.')
                 elif len(video_candidates) == 0:
                     logging.warning('No video found for this set.')
                 else:
                     joined_video_info = video_candidates[0]
                     orig_video_path = '{}/joined.mp4'.format(joined_video_info[0])
-                    new_video_path = '{}/{}_{}_{}.mp4'.format(output_folder, trip_code, set_code, camera)
-                    if os.path.isfile(orig_video_path):
-                        logging.info('Moving "{}" to "{}"'.format(orig_video_path, new_video_path))
-                        if os.path.isfile(new_video_path):
-                            logging.warning('Overwriting video "{}"'.format(new_video_path))
-                        shutil.move(orig_video_path, new_video_path)
-                        logging.info('Finished copying video for trip: {}, set: {}'.format(trip_code, set_code))
-                    else:
-                        logging.error('Video "{}" does not exist'.format(orig_video_path))
+                    rename_video(trip_code, set_code, camera, output_folder, orig_video_path)
             else:
                 logging.error('No video specified for this set.')
+
+def rename_video(trip_code, set_code, camera, output_folder, orig_video_path):
+    name_pieces = [trip_code, set_code]
+    if camera:
+        name_pieces.append(camera)
+    new_video_path = '{}/{}.mp4'.format(output_folder, '_'.join(name_pieces))
+    if os.path.isfile(orig_video_path):
+        logging.info('Moving "{}" to "{}"'.format(orig_video_path, new_video_path))
+        if os.path.isfile(new_video_path):
+            logging.warning('Overwriting video "{}"'.format(new_video_path))
+        shutil.move(orig_video_path, new_video_path)
+        logging.info('Finished copying video for trip: {}, set: {}'.format(trip_code, set_code))
+    else:
+        logging.error('Video "{}" does not exist'.format(orig_video_path))
 
 def get_cell_by_name_extractor(headers):
     extractor_func = lambda row, column_name: row[headers[column_name]]
