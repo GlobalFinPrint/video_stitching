01538d79dd6e4fe4acdb684cafd50d83fc78e73c
vkirkl <kirkl@vulcan.com>
2017-03-16 11:08:44 +0300

    bat with intro text

---
+++ bulk_stitch.bat
@@ -0,0 +1,25 @@
+@ echo off & cls
+echo *******************************************************************************************************
+echo *                                                                                                     *
+echo * For file stitching type the following at the prompt:                                                *
+echo *                                                                                                     *
+echo *  bulk_stitch.exe --rename_on_copy [unstitched files folder path] [stitched files destination path]  *
+echo *                                                                                                     *
+echo * For example, if your unstitched files are in directories within E:\my_raw_gopro_file_directory\     *
+echo *     and you want them to be stitched to D:\stitched\ then type this:                                *
+echo *                                                                                                     *     
+echo *  bulk_stitch.exe --rename_on_copy E:\my_raw_gopro_file_directory\ D:\stitched\                      *
+echo *                                                                                                     *
+echo *     ... and press Enter.                                                                            *
+echo *                                                                                                     *
+echo *                                                                                                     *
+echo * NOTE:  While this runs it will output lots of lines of (sometimes colorful) text.                   *
+echo *        Ignore all of that output and your stitched files should soon be in the destination          *
+echo *        directory!                                                                                   *
+echo *                                                                                                     *
+echo *******************************************************************************************************
+echo[
+echo[
+
+
+
