422a6024fdaea0e4d7b5c1a798c6d72c6b577de7
vkirkl <kirkl@vulcan.com>
2017-03-17 12:16:59 +0300

    working on build ...

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+from cx_Freeze import setup, Executable
 
+build_exe_options = {
+    "include_files": ["notes", "bulk_stitch.bat"],
+}
+
+executables = [
+    Executable('bulk_stitch.py')
+]
+
+setup(
+    name="finprint_video_stitching",
+    version="0.9",
+    description="Scripts for sticthing FinPrint GoPro files",
+    options={
+        "build_exe": build_exe_options
+    },
+    executables=executables,
+)
