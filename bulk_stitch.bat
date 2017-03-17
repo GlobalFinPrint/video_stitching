438dee43f25c0af428dc38c2a5834e2962ce339d
vkirkl <kirkl@vulcan.com>
2017-03-17 11:47:27 +0300

    remove the rename flag from the bat file instructions

--- bulk_stitch.bat
+++ bulk_stitch.bat
@@ -3,12 +3,12 @@ echo ***************************************************************************
 echo *                                                                                                     *
 echo * For file stitching type the following at the prompt:                                                *
 echo *                                                                                                     *
-echo *  bulk_stitch.exe --rename_on_copy [unstitched files folder path] [stitched files destination path]  *
+echo *  bulk_stitch.exe [unstitched files folder] [stitched files destination folder]  *
 echo *                                                                                                     *
 echo * For example, if your unstitched files are in directories within E:\my_raw_gopro_file_directory\     *
 echo *     and you want them to be stitched to D:\stitched\ then type this:                                *
 echo *                                                                                                     *     
-echo *  bulk_stitch.exe --rename_on_copy E:\my_raw_gopro_file_directory\ D:\stitched\                      *
+echo *  bulk_stitch.exe E:\my_raw_gopro_file_directory\ D:\stitched\                      *
 echo *                                                                                                     *
 echo *     ... and press Enter.                                                                            *
 echo *                                                                                                     *
